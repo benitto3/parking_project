80855dd6b21bab7861924d9174a88caa0d698bbe
benitto <dictionmail@proton.me>
2023-05-23 15:49:39 +0600

    Flask server works on other devices too now.

--- flask_web.py
+++ flask_web.py
@@ -7,4 +7,5 @@ def park():
     return render_template('parking_qazaq.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
+    # type host-ip:5000 to see the webapp on other devices
